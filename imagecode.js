20b7e40a8de495b6cd47f7f424ed8e7c67161321
Chris DeLeon <gamkedo@gmail.com>
2016-03-05 23:00:22 -0800

    placeholder images added for common game object types (toggle to show with E key), palette now in level editor which the player can select brushes from by clicking in the bar. Not yet placeable, rotateable or connected to the game types

---
+++ imagecode.js
@@ -0,0 +1,64 @@
+var levObjPics = [];
+
+var picsToLoad = 0;
+
+function countLoadedImageAndLaunchIfReady() {
+  picsToLoad--;
+  if(picsToLoad == 0) { // last image loaded?
+    loadingDoneSoStartGame();
+  }
+}
+
+function beginLoadingImage(imgVar, fileName) {
+  imgVar.onload=countLoadedImageAndLaunchIfReady;
+  imgVar.src="images/"+fileName;
+}
+
+function loadImageForTileCode(levObjCode, fileName) {
+  levObjPics[levObjCode] = document.createElement("img");
+  beginLoadingImage(levObjPics[levObjCode],fileName);
+}
+
+function loadImages() {
+
+  var imageList = [
+    /* {varName:logoImage, theFile:"logo.png"},*/ // for loading non-level parts
+    {levPartType:LEVELPART_ASTEROID, theFile:"asteroid.png"},
+    {levPartType:LEVELPART_CORE, theFile:"core.png"},
+    {levPartType:LEVELPART_LENS, theFile:"lens.png"},
+    {levPartType:LEVELPART_LENS2, theFile:"lens2.png"},
+    {levPartType:LEVELPART_LENS3, theFile:"lens3.png"},
+    {levPartType:LEVELPART_MIRROR, theFile:"mirror.png"},
+    {levPartType:LEVELPART_SOURCE, theFile:"source.png"},
+    {levPartType:LEVELPART_WALL, theFile:"wall.png"},
+    {levPartType:LEVELPART_DELETE, theFile:"delete.png"},
+    ];
+
+  picsToLoad = imageList.length;
+
+  for(var i=0;i<imageList.length;i++) {
+    if(imageList[i].levPartType != undefined) {
+      loadImageForTileCode(imageList[i].levPartType, imageList[i].theFile);
+    } else {
+      beginLoadingImage(imageList[i].varName, imageList[i].theFile);
+    } // end of else
+  } // end of for imageList
+
+} // end of function loadImages
+
+function drawBitmapCenteredAtLocationWithRotation(graphic, atX, atY,withAngle) {
+  ctx.save(); // allows us to undo translate movement and rotate spin
+  ctx.translate(atX,atY); // sets the point where our graphic will go
+  ctx.rotate(withAngle); // sets the rotation
+  ctx.drawImage(graphic,-graphic.width/2,-graphic.height/2); // center, draw
+  ctx.restore(); // undo the translation movement and rotation since save()
+}
+
+function drawBitmapFitIntoLocation(graphic, atX, atY, targetWid, targetHei) {
+  ctx.drawImage(graphic, // image (source)
+                          0,0, // source corner
+                          graphic.width,graphic.height, // source dimensions
+                          atX,atY, // dest corner
+                          targetWid, targetHei // dest dimensions
+                          );
+}
--- leveleditor.js
+++ leveleditor.js
@@ -1,10 +1,62 @@
+var levPartEnum = 0;
+const NO_SELECTION = -1;
+const LEVELPART_ASTEROID = (levPartEnum++);
+const LEVELPART_CORE = (levPartEnum++);
+const LEVELPART_LENS = (levPartEnum++);
+const LEVELPART_LENS2 = (levPartEnum++);
+const LEVELPART_LENS3 = (levPartEnum++);
+const LEVELPART_MIRROR = (levPartEnum++);
+const LEVELPART_SOURCE = (levPartEnum++);
+const LEVELPART_WALL = (levPartEnum++);
+const LEVELPART_DELETE = (levPartEnum++);
+const LEVELPART_ENUM_KINDS = (levPartEnum++);
+
+const UI_BUTTON_PIXEL_SIZE = 40;
+
 var isInEditor = false;
+var editorMouseOverItem = NO_SELECTION;
+var editorSelectedBrush = NO_SELECTION;
 
 function toggleEditor() {
 	isInEditor = !isInEditor;
 }
 
-function editorDraw() {
-	colorRect(0,0,canvas.width,canvas.height,'red');
-	colorText("now in editor (placeholder), mouse at (" +mouseX+", "+mouseY+")" , 15, 15, 'black');
+function editorMouseClicked() {
+	if(editorMouseOverItem != NO_SELECTION) {
+		editorSelectedBrush = editorMouseOverItem;
+	}
+}
+
+function editorUpdate() { // can contain logic, too
+	// logic code for editor
+	if(mouseX > canvas.width-UI_BUTTON_PIXEL_SIZE) {
+		editorMouseOverItem = Math.floor( mouseY / UI_BUTTON_PIXEL_SIZE ); 
+		if(editorMouseOverItem < 0 || editorMouseOverItem > LEVELPART_DELETE) {
+			editorMouseOverItem = NO_SELECTION;
+		}
+	} else {
+		editorMouseOverItem = NO_SELECTION;
+	}
+
+	// draw code for editor
+	var xCoord = canvas.width-UI_BUTTON_PIXEL_SIZE;
+	for(var i=0;i<LEVELPART_ENUM_KINDS;i++) {
+		var yCoord = UI_BUTTON_PIXEL_SIZE*i;
+		if(editorSelectedBrush == i) {
+			colorRect(xCoord, yCoord, UI_BUTTON_PIXEL_SIZE,UI_BUTTON_PIXEL_SIZE, '#00ffff');
+		} else if(editorMouseOverItem == i) {
+			strokeRect(xCoord, yCoord, UI_BUTTON_PIXEL_SIZE,UI_BUTTON_PIXEL_SIZE, 'red', 4);
+		} else {
+			strokeRect(xCoord, yCoord, UI_BUTTON_PIXEL_SIZE,UI_BUTTON_PIXEL_SIZE, 'white', 1);
+		}
+
+		drawBitmapFitIntoLocation(levObjPics[i],
+			xCoord, yCoord,
+			UI_BUTTON_PIXEL_SIZE,UI_BUTTON_PIXEL_SIZE);
+	}
+	if(editorSelectedBrush != NO_SELECTION && editorMouseOverItem == NO_SELECTION) {
+		drawBitmapCenteredAtLocationWithRotation(levObjPics[editorSelectedBrush],
+													mouseX,mouseY, 0);
+	}
+	colorText("Editor state debug output (placeholder)", 15, 15, 'white');
 }
