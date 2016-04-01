4102e625e6fce623563f503bb002fea01aa66dd1
dalath <info@dalath.com>
2016-04-01 18:02:25 +0200

    The original "test case" of beams now gets re-created every time you exit the editor

--- leveleditor.js
+++ leveleditor.js
@@ -197,8 +197,8 @@ LevelEditor.toggle = function() {
 	LevelEditor.active = !LevelEditor.active;
 	LevelEditor.selectedPiece = null;
 	if(LevelEditor.active) {
-		var typesNotHandled = [].concat(currentLevel.beams, currentLevel.lasers, currentLevel.blocks, currentLevel.mirrors, currentLevel.cores, currentLevel.coresinks, currentLevel.sprites);
-		// Handled: currentLevel.lenses
+		var typesNotHandled = [].concat(currentLevel.lasers, currentLevel.blocks, currentLevel.mirrors, currentLevel.cores, currentLevel.coresinks, currentLevel.sprites);
+		// Handled: currentLevel.lenses, TMP handled: currentLevel.beams
 		currentLevel = new OptiLevel.init();
 		currentLevel.addManyOpticsPieces(typesNotHandled);
 	} else {
@@ -209,6 +209,23 @@ LevelEditor.toggle = function() {
 				currentLevel.addOpticsPiece(new Lens(points, 1.3, LENS_COLOR));
 			}
 		}
+
+
+		/** TEMP SOLUTION **/
+		var trailLength = 10;
+		var dashLineWidth = 2;
+		var beam1 = new LaserBeam(650,325, LIGHTSPEED, 180, trailLength, 'red', dashLineWidth);
+		var beam2 = new LaserBeam(650,375, LIGHTSPEED, 180, trailLength, 'red', dashLineWidth);
+		var beam3 = new LaserBeam(650,425, LIGHTSPEED, 180, trailLength, 'red', dashLineWidth);
+		var beam4 = new LaserBeam(650,475, LIGHTSPEED, 180, trailLength, 'red', dashLineWidth);
+		var beam5 = new LaserBeam(400,150, LIGHTSPEED, -15, trailLength, 'red', dashLineWidth);
+		var beam6 = new LaserBeam(400,150, LIGHTSPEED, -10, trailLength, 'green', dashLineWidth);
+		var beam7 = new LaserBeam(400,150, LIGHTSPEED, -5, trailLength, 'blue', dashLineWidth);
+		var beam8 = new LaserBeam(400,150, LIGHTSPEED, 0, trailLength, 'purple', dashLineWidth);
+		currentLevel.addManyOpticsPieces([beam1,beam2,beam3,beam4,beam5,beam6,beam7,beam8]);
+		/** END **/
+
+
 	}
 }
 
